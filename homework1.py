7c2ea43ea3ad07c9ab38dfb19d82325e92cc026b
Anton <tymoshenko.a@gmail.com>
2020-12-21 22:03:36 +0200

    First and second homeworks

---
+++ homework1.py
@@ -0,0 +1,58 @@
+
+
+def parse(query: str) -> dict:
+    new_dict = {}
+    if '?' not in query:
+        return new_dict
+    else:
+        params = query.split('?')
+        query = params[1]
+        params = query.split('&')
+        for i in params:
+            if i != '':
+                item = i.split('=')
+                new_dict[item[0]] = item[1]
+        return new_dict
+
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?name=Dima&age=28') == {'name': 'Dima', 'age': '28'}
+    assert parse('https://mail.google.com/mail/u/0/#inbox/FMfcgxwKjxBkvzkhSlzNbDFjHmBJfSnJ') == {}
+    assert parse('https://') == {}
+    assert parse('http://example.com/?name=Dima&age=28&birthday=06.08.1979') == {'name': 'Dima', 'age': '28',
+                                                                                 'birthday': '06.08.1979'}
+    assert parse('http://example.com/?name=Dima&age=28&birthday=06.08.1979&') == {'name': 'Dima', 'age': '28',
+                                                                                 'birthday': '06.08.1979'}
+
+
+def parse_cookie(query: str) -> dict:
+    new_dict = {}
+    params = query.split(';')
+    for i in params:
+        if i != '':
+            item = i.split('=')
+            if len(item) > 2:
+                new_item = [item[0], item[1]]
+                new_dict[new_item[0]] = '='.join(item[1:])
+            elif len(item) == 1:
+                new_dict[item[0]] = 'Empty!!!'
+            else:
+                new_dict[item[0]] = item[1]
+    return new_dict
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=User;age=28;birthday=06.08.1979') == {'name': 'User', 'age': '28', 'birthday': '06.08.1979'}
+    assert parse_cookie('name=User;age=28;email=mail@mail.com') == {'name': 'User', 'age': '28', 'email': 'mail@mail.com'}
+    assert parse_cookie('name=Dima=User=test;age=28;') == {'name': 'Dima=User=test', 'age': '28'}
+    assert parse_cookie('name;age=28;') == {'name': 'Empty!!!', 'age': '28'}
+    assert parse_cookie('name;age') == {'name': 'Empty!!!', 'age': 'Empty!!!'}
---
+++ homework2.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from faker import Faker
+import pandas as pd
+import requests
+
+fake = Faker()
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/requirements/')
+def requirements():
+    req_data = open('requirements.txt', 'r')
+    return req_data.read()
+
+
+@app.route('/generate-users/')
+def generate():
+    users = {}
+    user_count = request.args.get('user_count', 100)
+    for i in range(int(user_count)):
+        users[fake.name()] = fake.ascii_email()
+    return str(users)
+
+
+@app.route('/mean/')
+def mean():
+    data = pd.read_csv('hw.csv')
+    height = data[' "Height(Inches)"'].mean() * 2.54
+    weight = data[' "Weight(Pounds)"'].mean() * 0.453592
+    return 'Height: ' + str(height) + ', Weight: ' + str(weight)
+
+
+@app.route('/space/')
+def astro_qty():
+    astro = requests.get('http://api.open-notify.org/astros.json')
+    qty = astro.json()['number']
+    return str(qty)
+
+
